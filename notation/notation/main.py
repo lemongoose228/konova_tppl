3d16b6b5dc7f8550d496ff084c8bc93a282fcae4
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-11-09 19:55:33 +0800

    Добавлен код для notation

--- notation/notation/main.py
+++ notation/notation/main.py
@@ -1,1 +1,28 @@
+class dataError(Exception):
+    pass
+
+def is_operator(symbol):
+    return symbol in ('+', '-', '*', '/')
+
+def prefix_to_infix(data):
+    symbols = data.split()
+    res = []
+
+    for symbol in reversed(symbols):
+        if is_operator(symbol):
+            try:
+                num1 = res.pop()
+                num2 = res.pop()
+            except IndexError:
+                raise dataError("Количество цифр не соответствует количеству операторов")
+
+            new_data = f"({num1} {symbol} {num2})"
+            res.append(new_data)
+        else:
+            res.append(symbol)
+
+    if len(res) < 1:
+        raise dataError("Ничего небыло введено")
+        
+    return res.pop()
 
